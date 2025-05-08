f9f2b03a8661b66cab07436d2b0d4de12d561280
Xavier Hueber <xavier.hueber@tutanota.de>
2025-05-08 11:04:11 +0200

    Add support for listening timestamp and duration

--- deezer_history.py
+++ deezer_history.py
@@ -1,7 +1,10 @@
 import requests
+from datetime import datetime
+import pytz
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux i686; rv:135.0) Gecko/20100101 Firefox/135.0"
 HISTORY_SIZE = '100'
+TZ = 'Europe/Zurich' #Timezone
 ARL = ''
 session = None
 
@@ -33,7 +36,8 @@ def get_history() -> None:
     history = session.post('https://www.deezer.com/ajax/gw-light.php?method=user.getSongsHistory&input=3&api_version=1.0&api_token={}'.format(csrf_token), json={'nb': HISTORY_SIZE, 'start': '0'})
     history_json = history.json()['results']
     for song in history_json['data']:
-        print(song['SNG_TITLE'] + ' - ' + song['ALB_TITLE'] + ' from ' + song['ART_NAME'] )
+        date = datetime.fromtimestamp(song['TS'], pytz.timezone(TZ)).strftime('%d-%m-%Y %H:%M:%S')
+        print(song['SNG_TITLE'] + ' - ' + song['ALB_TITLE'] + ' from ' + song['ART_NAME'] + ' | Started at ' + date  + ' and listened ' + song['DURATION'] + 's.')
 
 if __name__ == '__main__':
     init_deezer_session()
